0c8cffa8fccf605b283e1f43f277fcc3647717d8
LuaGanrqunx <aqunxgm@gmail.com>
2019-06-03 20:54:31 +0800

    A is adding some more work!!

--- git_test/src/git_test/newfile/AddNewFeature.java
+++ git_test/src/git_test/newfile/AddNewFeature.java
@@ -4,6 +4,7 @@ public class AddNewFeature {
 
 	public static void main(String[] args) {
 		System.out.println("B is adding some new features...");
+		System.out.println("A is adding some more work!!");
 	}
 
 }
